101da3989a69fb96cd04d358ab2aa64b7e74dc04
Eric Weidow <eric@weidow.se>
2022-07-13 12:22:30 +0200

    Change all " to '

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 function playRound(playerSelection, computerSelection) {
     let playerSelectionIndex = selectionIndex(playerSelection);
-    console.log("Player index: " + playerSelectionIndex);
+    console.log('Player index: ' + playerSelectionIndex);
 
     let computerSelectionIndex = selectionIndex(computerSelection);
-    console.log("Computer index: " + computerSelectionIndex);
+    console.log('Computer index: ' + computerSelectionIndex);
 
     let diff = playerSelectionIndex - computerSelectionIndex;
-    console.log("Diff: " + diff);
+    console.log('Diff: ' + diff);
 
     let modulu = diff % 3;
-    console.log("Modulu 3: " + modulu);
+    console.log('Modulu 3: ' + modulu);
     if (modulu < 0) { modulu += 3; }
     
     switch (modulu) {
@@ -23,7 +23,7 @@ function playRound(playerSelection, computerSelection) {
             console.log(`You lose! ${firstLetterCapitalised(computerSelection)} beats ${playerSelection}.`);
             return -1;
         default:
-            console.log("Something went wrong!");
+            console.log('Something went wrong!');
             return 0;
     }
 }
@@ -34,14 +34,14 @@ function computerPlay() { // Random choice between rock (1), paper (2) and sciss
 
 function selectionIndex(selection) { // Convert string to a number
     switch(selection) {
-        case "rock":
+        case 'rock':
             return 1;
-        case "paper":
+        case 'paper':
             return 2;
-        case "scissors":
+        case 'scissors':
             return 3;
         default:
-            console.log("Something went wrong!");
+            console.log('Something went wrong!');
             return -1;
     }
 }
@@ -49,19 +49,19 @@ function selectionIndex(selection) { // Convert string to a number
 function selectionName(selection) { // Convert number to a string
     switch(selection) {
         case 1:
-            return "rock";
+            return 'rock';
         case 2:
-            return "paper";
+            return 'paper';
         case 3:
-            return "scissors";
+            return 'scissors';
         default:
-            console.log("Something went wrong!");
+            console.log('Something went wrong!');
             return -1;
     }
 }
 
 function firstLetterCapitalised(input) { // Conert string to 
-    if(typeof input === "string") {
+    if(typeof input === 'string') {
         if(input.length > 1) {
             firstLetter = input.charAt(0).toUpperCase();
             restOfWord = input.substring(1).toLowerCase();
@@ -75,13 +75,13 @@ function firstLetterCapitalised(input) { // Conert string to
 // Player Selection (Do-while loop to only allow valid inputs)
 let playerSelection;
 do {
-    playerSelection = prompt("Rock, paper or scissors? ", "").toLowerCase();
-} while(!(playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors"));
-console.log("Player chose: " + playerSelection);
+    playerSelection = prompt('Rock, paper or scissors? ', '').toLowerCase();
+} while(!(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors'));
+console.log('Player chose: ' + playerSelection);
 
 // Computer Selection
 let computerSelection = selectionName(computerPlay());
-console.log("Computer chose: " + computerSelection);
+console.log('Computer chose: ' + computerSelection);
 
 // Playing a round
 console.log(playRound(playerSelection, computerSelection));

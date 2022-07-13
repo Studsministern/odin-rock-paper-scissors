e09ef131282e1ca77e0d4a936e05d207e9ee0c8b
Eric Weidow <eric@weidow.se>
2022-07-13 13:24:18 +0200

    Clarify functions, function names and comments

--- js/script.js
+++ js/script.js
@@ -2,44 +2,41 @@ function game() {
     let playerScore = 0;
     let computerScore = 0;
 
-    for(let round = 1; round <= 5; round++) { // 5 rounds
+    for(let round = 1; round <= 5; round++) {
         let scoreChange = 0;
         
         do {
-            // Player and computer selections
-            let playerSelection;
-            do {
-                playerSelection = prompt('Rock, paper or scissors? ', '').toLowerCase();
-            } while(!(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors')); // Do-while loop to only allow valid inputs
-
-            let computerSelection = selectionName(computerPlay());
-
-            // Playing a round
-            scoreChange = playRound(round, playerSelection, computerSelection);
-        } while (scoreChange === 0); //Do-while loop to replay rounds with a tie!
+            scoreChange = playOneRound(round, playerSelection, computerSelection);
+        } while (scoreChange === 0); // Replay rounds with a tie!
         
-        if(scoreChange > 0) { playerScore++; }
-        if(scoreChange < 0) { computerScore++; }
+        if(scoreChange > 0) { playerScore++; }   // Positive => Player won the round
+        if(scoreChange < 0) { computerScore++; } // Negative => Computer won the round
     }
     
-    if(playerScore > computerScore) { console.log(`You won the game! The score was ${playerScore}|${computerScore}.`); }
-                              else { console.log(`You lost the game! The score was ${playerScore}|${computerScore}.`); }
+    if(playerScore > computerScore) { console.log(`You won the game! The score was ${playerScore}|${computerScore}.`); } // Win message
+                              else { console.log(`You lost the game! The score was ${playerScore}|${computerScore}.`); } // Lose message
 }
 
-function playRound(round, playerSelection, computerSelection) {
-    // Modulo operation to compare selections
-    let modulo = (selectionIndex(playerSelection) - selectionIndex(computerSelection)) % 3;
-    if (modulo < 0) { modulo += 3; } // Positive values only requires 3 cases (0, 1, 2) instead of 5 (-2, -1, 0, 1, 2)
+function playOneRound(round) {
+    // Computer and player selections
+    let computerSelection = convertSelectionIndexToName(randomizeComputerSelection());
+    let playerSelection;
+    do {
+        playerSelection = prompt('Rock, paper or scissors? ', '').toLowerCase();
+    } while(!(playerSelection === 'rock' || 
+              playerSelection === 'paper' || 
+              playerSelection === 'scissors')); // Only allows valid inputs
     
-    switch (modulo) { // Compare selections and returns points for the player
+    // Logs the result and returns points
+    switch (compareSelections(playerSelection, computerSelection)) {
         case 0:
             console.log(`Round ${round}: Tie! Both players chose ${playerSelection}. Play the round again!`);
             return 0; // 0 points if it is a tie
         case 1:
-            console.log(`Round ${round}: You win! ${firstLetterCapitalised(playerSelection)} beats ${computerSelection}.`);
+            console.log(`Round ${round}: You win! ${capitalizeFirstLetterOfString(playerSelection)} beats ${computerSelection}.`);
             return 1; // 1 point if the player wins
         case 2:
-            console.log(`Round ${round}: You lose! ${firstLetterCapitalised(computerSelection)} beats ${playerSelection}.`);
+            console.log(`Round ${round}: You lose! ${capitalizeFirstLetterOfString(computerSelection)} beats ${playerSelection}.`);
             return -1; // -1 point if the player loses
         default:
             console.log(`Round ${round}: Something went wrong!`);
@@ -47,11 +44,17 @@ function playRound(round, playerSelection, computerSelection) {
     }
 }
 
-function computerPlay() { // Random choice between rock (1), paper (2) and scissors (3)
+function compareSelections(playerSelection, computerSelection) {
+    let modulo = (convertSelectionNameToIndex(playerSelection) - convertSelectionNameToIndex(computerSelection)) % 3;
+    if (modulo < 0) { modulo += 3; } // Positive values only requires 3 cases (0, 1, 2) instead of 5 (-2, -1, 0, 1, 2)
+    return modulo;
+}
+
+function randomizeComputerSelection() { // Random choice between rock (1), paper (2) and scissors (3)
     return Math.floor(Math.random() * 3) + 1;
 }
 
-function selectionIndex(selection) { // Convert string to a number
+function convertSelectionNameToIndex(selection) {
     switch(selection) {
         case 'rock':
             return 1;
@@ -65,7 +68,7 @@ function selectionIndex(selection) { // Convert string to a number
     }
 }
 
-function selectionName(selection) { // Convert number to a string
+function convertSelectionIndexToName(selection) {
     switch(selection) {
         case 1:
             return 'rock';
@@ -79,7 +82,7 @@ function selectionName(selection) { // Convert number to a string
     }
 }
 
-function firstLetterCapitalised(input) { // Conert string to 
+function capitalizeFirstLetterOfString(input) {
     if(typeof input === 'string') {
         if(input.length > 1) {
             firstLetter = input.charAt(0).toUpperCase();

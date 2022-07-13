b781f4ac3994497d1a42c75865fe47d9d51d1036
Eric Weidow <eric@weidow.se>
2022-07-13 12:52:03 +0200

    Implement game() function and multiple rounds
    The game function contains player- and computer score. A for-loop allows
    the game to be played for 5 rounds, where each round contains the
    selections and comparisons. A round will be replayed every time there is
    a tie. The final score is displayed along with who the winner is.
    
    The playRound() function has been changed to take the round number as a
    parameter and include it in the result message for each round.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,48 @@
-function playRound(playerSelection, computerSelection) {
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let round = 1; round <= 5; round++) { // 5 rounds
+        let scoreChange = 0;
+        
+        do {
+            // Player and computer selections
+            let playerSelection;
+            do {
+                playerSelection = prompt('Rock, paper or scissors? ', '').toLowerCase();
+            } while(!(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors')); // Do-while loop to only allow valid inputs
+
+            let computerSelection = selectionName(computerPlay());
+
+            // Playing a round
+            scoreChange = playRound(round, playerSelection, computerSelection);
+        } while (scoreChange === 0); //Do-while loop to replay rounds with a tie!
+        
+        if(scoreChange > 0) { playerScore++; }
+        if(scoreChange < 0) { computerScore++; }
+    }
+    
+    if(playerScore > computerScore) { console.log(`You won the game! The score was ${playerScore}|${computerScore}.`); }
+                              else { console.log(`You lost the game! The score was ${playerScore}|${computerScore}.`); }
+}
+
+function playRound(round, playerSelection, computerSelection) {
     // Modulo operation to compare selections
     let modulo = (selectionIndex(playerSelection) - selectionIndex(computerSelection)) % 3;
     if (modulo < 0) { modulo += 3; } // Positive values only requires 3 cases (0, 1, 2) instead of 5 (-2, -1, 0, 1, 2)
     
     switch (modulo) { // Compare selections and returns points for the player
         case 0:
-            console.log(`Tie! Both players chose ${playerSelection}.`);
+            console.log(`Round ${round}: Tie! Both players chose ${playerSelection}. Play the round again!`);
             return 0; // 0 points if it is a tie
         case 1:
-            console.log(`You win! ${firstLetterCapitalised(playerSelection)} beats ${computerSelection}.`);
+            console.log(`Round ${round}: You win! ${firstLetterCapitalised(playerSelection)} beats ${computerSelection}.`);
             return 1; // 1 point if the player wins
         case 2:
-            console.log(`You lose! ${firstLetterCapitalised(computerSelection)} beats ${playerSelection}.`);
+            console.log(`Round ${round}: You lose! ${firstLetterCapitalised(computerSelection)} beats ${playerSelection}.`);
             return -1; // -1 point if the player loses
         default:
-            console.log('Something went wrong!');
+            console.log(`Round ${round}: Something went wrong!`);
             return 0;
     }
 }
@@ -63,16 +91,4 @@ function firstLetterCapitalised(input) { // Conert string to
     return;
 }
 
-// Player Selection (Do-while loop to only allow valid inputs)
-let playerSelection;
-do {
-    playerSelection = prompt('Rock, paper or scissors? ', '').toLowerCase();
-} while(!(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors'));
-console.log('Player chose ' + playerSelection);
-
-// Computer Selection
-let computerSelection = selectionName(computerPlay());
-console.log('Computer chose ' + computerSelection);
-
-// Playing a round
-console.log(playRound(playerSelection, computerSelection));
+game();

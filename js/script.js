07485a0ea1b3de1bbce67c04f67e0d5ba111c055
Eric Weidow <eric@weidow.se>
2022-07-13 11:16:11 +0200

    Add player selection and case insensitivity
    Player selection is done by using a prompt and checking for valid inputs
    in a do-while loop. .toLowerCase() is used for the player selection. The
    computer selection instead has all lower characters.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 function computerPlay() {
     switch(Math.floor(Math.random() * 3)) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
         default:
             return "Something went wrong!";
     }
 }
 
+// Player Selection (Do-while loop to only allow valid inputs)
+let playerSelection;
+do {
+    playerSelection = prompt("Rock, paper or scissors? ", "").toLowerCase();
+} while(!(playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors"));
+console.log(playerSelection);
+
+// Computer Selection
+let computerSelection = computerPlay();
 console.log(computerPlay());
